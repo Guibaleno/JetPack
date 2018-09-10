ba5a1717828bd109c1a01b0e9ba0db09ce403faa
B533P-21\533 <glambertgm@gmail.com>
2018-09-10 16:45:32 -0400

    Mouvement de base et coin is appearing

---
+++ New Unity Project/Assets/Scripts/Coins/CoinAppearing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAppearing : MonoBehaviour {
+   // [SerializeField] Rigidbody2D Player;
+   // [SerializeField] Rigidbody2D Coins;
+    // Use this for initialization
+    void Start () {
+    //    Player = GetComponent<Rigidbody2D>();
+    //    Coins = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
